f82c84834fa569fc387e00ac18d39f9ce8ed12d0
adam.quinnell@ada.ac.uk <adam.quinnell@ada.ac.uk>
2026-06-15 14:13:20 +0000

    Created LICENSE.txt; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,65 @@ class Form1(Form1Template):
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("add_score", "click")
+  def add_score_click(self, **event_args):
+    score = int(self.player_score.content)
+    score = score + 1
+    self.player_score.content = str(score)
+
+  @handle("add2_score", "click")
+  def add2_score_click(self, **event_args):
+    score = int(self.player2_score.content)
+    score = score + 1
+    self.player2_score.content = str(score)
+
+  @handle("add3_score", "click")
+  def add3_score_click(self, **event_args):
+    score = int(self.player3_score.content)
+    score = score + 1
+    self.player3_score.content = str(score)
+
+  @handle("add4_score", "click")
+  def add4_score_click(self, **event_args):
+    score = int(self.player_score.content)
+    score = score + 1
+    self.player_score.content = str(score)
+
+  @handle("sub_score", "click")
+  def sub_score_click(self, **event_args):
+    
+    score = int(self.player_score.content)
+    score = score - 1
+    self.player_score.content = str(score)
+    if score == -1:
+      score = 0
+      self.player_score.content = str(score)
+
+  @handle("sub2_score", "click")
+  def sub2_score_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    score = int(self.player2_score.content)
+    score = score - 1
+    self.player2_score.content = str(score)
+    if score == -1:
+      score = 0
+      self.player2_score.content = str(score)
+
+  @handle("sub3_score", "click")
+  def sub3_score_click(self, **event_args):
+    score = int(self.player3_score.content)
+    score = score - 1
+    self.player3_score.content = str(score)
+    if score == -1:
+      score = 0
+      self.player3_score.content = str(score)
+
+  @handle("sub4_score", "click")
+  def sub4_score_click(self, **event_args):
+    score = int(self.player4_score.content)
+    score = score - 1
+    self.player4_score.content = str(score)
+    if score == -1:
+      score = 0
+      self.player4_score.content = str(score)
